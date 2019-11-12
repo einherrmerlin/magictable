381c88838f254e44298518cd70936bc718644c6e
Merlin Müller <merlin.mueller@2mt.info>
2019-11-12 16:58:46 +0100

    Add more code

---
+++ constants.py
@@ -0,0 +1,11 @@
+import constants
+
+PROGRAM_EXIT = "beenden"
+FULLTIME = 13
+JAN = ["impedimenta", 3.1, "expelliarmus", 3.0]
+MERLIN = ["avada kedavra", 3.5, "expecto patronum", 3.1]
+CELINA = ["stupor", 2.9, "lumus maximus", 3.4]
+STOP_SLEEP_DURATION = 0.5
+RELAIS_1_GPIO = 17
+RELAIS_2_GPIO = 18
+GPIO_CLEAN = False
--- voicerecognition.py
+++ voicerecognition.py
@@ -1,48 +1,8 @@
-import random
 import time
 import speech_recognition as sr
-import RPi.GPIO as GPIO
-
-# set GPIO Pins
-GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-RELAIS_1_GPIO = 17
-RELAIS_2_GPIO = 18
-
-# set local variables
-tup = "aufwärts"
-tdown = "abwärts"
-tname = "banane"
-merlinsit = "banane waschbär sitzen"
-merlinstand = "banane waschbär stehen"
-jansit = "banane ameisenbär sitzen"
-janstand = "banane ameisenbär stehen"
-jansitpos = 3.1
-merlinsitpos = 3.5
-merlinstandpos = 3.0
-janstandpos = 3.2
-
-
-def gpio_setup():
-    GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
-    GPIO.setup(RELAIS_2_GPIO, GPIO.OUT)
-
-
-def table_stop():
-    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
-    GPIO.output(RELAIS_2_GPIO, GPIO.HIGH)
-
-
-def table_down():
-    GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
-
-
-def table_up():
-    GPIO.output(RELAIS_2_GPIO, GPIO.LOW)
-
-
-def gpio_cleanup():
-    GPIO.cleanup()
+import constants as c
+import tablecontrol as tc
+import led
 
 
 def recognize_speech_from_mic(recognizer, microphone):
@@ -56,7 +16,9 @@ def recognize_speech_from_mic(recognizer, microphone):
     # adjust the recognizer sensitivity to ambient noise and record audio
     # from the microphone
     with microphone as source:
+        print ("Lade Welt - Bitte warten")
         recognizer.adjust_for_ambient_noise(source)
+        print ("Bitte jetzt sprechen")
         audio = recognizer.listen(source)
 
     try:
@@ -70,73 +32,33 @@ def recognize_speech_from_mic(recognizer, microphone):
     return text
 
 
-def table_merlin_sit():
-    table_down()
-    time.sleep(13)
-    table_stop()
-    time.sleep(1)
-    table_up()
-    time.sleep(3.5)
-    table_stop()
-
-
-def table_merlin_stand():
-    table_up()
-    time.sleep(13)
-    table_stop()
-    time.sleep(1)
-    table_down()
-    time.sleep(3)
-    table_stop()
-
-
-def table_jan_sit():
-    table_down()
-    time.sleep(13)
-    table_stop()
-    time.sleep(1)
-    table_up()
-    time.sleep(3.1)
-    table_stop()
-
-
-def table_jan_stand():
-    table_up()
-    time.sleep(13)
-    table_stop()
-    time.sleep(1)
-    table_down()
-    time.sleep(3.1)
-    table_stop()
-
 if __name__ == "__main__":
 	# create recognizer and mic instancve
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-    gpio_setup()
+    tc.gpio_cleanup()
+    tc.gpio_setup()
+    tc.table_calibrate() # sobald diese funktion aufgerufen wird kommt ein Error
 
     # recognize speech and print speech
-    text = recognize_speech_from_mic(recognizer, microphone)
-    text = text.lower()
-    listen = True
-    while listen == True
-        if (text) == merlinsit:
-            print ("Ich habe verstanden: " + text)
-            table_merlin_sit()
-            print ("Tisch an Position Merlin - Sitzen gefahren")
-        elif (text) == merlinstand:
-            print ("Ich habe verstanden: " + text)
-            table_merlin_stand()
-            print ("Tisch an Position Merlin - Stehen gefahren")
-        elif (text) == jansit:
-            print ("Ich habe verstanden: " + text)
-            table_jan_sit()
-            print ("Tisch an Position Jan - Sitzen gefahren")
-        elif (text) == janstand:
-            print ("Ich habe verstanden: " + text)
-            table_jan_stand()
-            print ("Tisch an Position Jan - Stehen gefahren")
+    while True:
+        recognizer = sr.Recognizer()
+        microphone = sr.Microphone()
+        text = recognize_speech_from_mic(recognizer, microphone)
+        text = text.lower()
+        if (text) == c.MERLIN[0]:
+            print ("~~~ " + text + " ~~~ - Jan / sitzen")
+            tc.table_sitposition(c.MERLIN[1])
+        elif (text) == c.MERLIN[2]:
+            print ("~~~ " + text + " ~~~ - Merlin / stehen")
+            tc.table_standposition(c.MERLIN[3])
+        elif (text) == c.JAN[0]:
+            print ("~~~ " + text + " ~~~ - Jan / sitzen")
+            tc.table_sitposition(c.JAN[1])
+        elif (text) == c.JAN[2]:
+            print ("~~~ " + text + " ~~~ - Jan / stehen")
+            tc.table_standposition(c.JAN[3])
+        elif text == c.PROGRAM_EXIT:
+            print (text + " - Programm beendet")
+            tc.gpio_cleanup()
+            break
         else:
-            print (text)
-            gpio_cleanup()
-            print ("Keine Auswahl getroffen - Programm beendet")
+            print (text + " - Bitte Eingabe wiederholen")
